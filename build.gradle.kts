b29df740ecf0deaaf0440db1db7dc7cfcbf0cc13
Gustavo Almeida <gualmeida2004@gmail.com>
2023-06-08 11:59:05 -0300

    Configurar docker-compose-module apenas em desenvolvimento

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-docker-compose")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
